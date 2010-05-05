de36e255f6382f90f5f9a116e0806689d6a7c3a2
Chetan Sarva <chetan@pixelcop.net>
2010-05-05 18:36:46 -0400

    import script for json data

---
+++ app/models/restaurant.rb
@@ -0,0 +1,4 @@
+
+class Restaurant < ActiveRecord::Base
+	
+end
--- script/lunch/import_data.rb
+++ script/lunch/import_data.rb
@@ -10,15 +10,37 @@ require 'json'
 require 'pp'
 
 data = JSON.load(File.new(RAILS_TMP + "/data.json"))
-data['feed'].keys # => ["category", "author", "title", "openSearch$startIndex", "id", "openSearch$totalResults", "xmlns$gsx", "xmlns$openSearch", "entry", "link", "updated", "xmlns"]
 
-data['feed']['entry'].first # => {"gsx$category"=>{"$t"=>"Carts,Street_Meat,Salads"}, "gsx$longitude"=>{"$t"=>"-73.972084"}, "category"=>[{"term"=>"http://schemas.google.com/spreadsheets/2006#list", "scheme"=>"http://schemas.google.com/spreadsheets/2006"}], "gsx$phone"=>{"$t"=>""}, "title"=>{"$t"=>"-73.972084", "type"=>"text"}, "gsx$description"=>{"$t"=>"Best greek salad from a cart"}, "gsx$zipcode"=>{"$t"=>""}, "gsx$title"=>{"$t"=>"100% Halal & Healthy Cart"}, "id"=>{"$t"=>"http://spreadsheets.google.com/feeds/list/pp3LMci9UjS4MwJEjJFO_Ww/default/public/values/40qnt"}, "gsx$closed"=>{"$t"=>""}, "gsx$address"=>{"$t"=>""}, "gsx$latitude"=>{"$t"=>"40.760571"}, "gsx$xstreet"=>{"$t"=>"55th St. btw. Mad+Park"}, "content"=>{"$t"=>"latitude: 40.760571, title: 100% Halal & Healthy Cart, xstreet: 55th St. btw. Mad+Park, description: Best greek salad from a cart, category: Carts,Street_Meat,Salads, link: http://midtownlunch.com/category/100-halal-healthy/", "type"=>"text"}, "link"=>[{"href"=>"http://spreadsheets.google.com/feeds/list/pp3LMci9UjS4MwJEjJFO_Ww/default/public/values/40qnt", "rel"=>"self", "type"=>"application/atom+xml"}], "gsx$link"=>{"$t"=>"http://midtownlunch.com/category/100-halal-healthy/"}, "updated"=>{"$t"=>"2010-03-20T18:10:30.876Z"}}
+def extract(t)
+    ret = {}
+    t.keys.each { |k| 
+        if k =~ /^gsx\$(.+)$/ then
+            ret[$1] = t[k]["$t"].strip
+        end
+    }
+    ret
+end
 
 locations = data['feed']['entry']
 
-test = locations.first
-test.keys # => ["gsx$category", "gsx$longitude", "category", "gsx$phone", "title", "gsx$description", "gsx$zipcode", "gsx$title", "id", "gsx$closed", "gsx$address", "gsx$latitude", "gsx$xstreet", "content", "link", "gsx$link", "updated"]
+locations.each do |loc|
 
-pp test
-pp test['content']
-pp test['gsx$longitude']
+    c = extract(loc)
+
+    r = Restaurant.new
+    r.name = c["title"]
+    r.categories = c["category"]
+    r.address = c["address"]
+    r.xstreet = c["xstreet"]
+    r.lat = c["latitude"]
+    r.lng = c["longitude"]
+    r.zip = c["zipcode"]
+    r.phone = c["phone"]
+    r.description = c["description"]
+    r.link = c["link"]
+    r.closed = (c["closed"] == "TRUE" ? 1 : 0)
+    r.updated = loc["updated"]["$t"]
+
+    puts "saving: #{r.name}"
+    r.save!
+end
