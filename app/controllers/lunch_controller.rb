acc9ba5a05d2000614e33f2e88cd67190fee668f
Chetan Sarva <chetan@pixelcop.net>
2010-05-06 08:58:56 -0400

    initial lookup by location

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -4,7 +4,17 @@ class LunchController < ApplicationController
     include Geokit::IpGeocodeLookup
    
     def index
-        puts session[:geo_location]
+        session[:geo_location] = Geokit::Geocoders::GoogleGeocoder.geocode('450 Lexington Avenue, New York, NY 10017') if not session[:geo_location]
+        # p session[:geo_location]
+        
+        if session[:geo_location] then
+            
+            srand Time.new.to_i
+            
+            near = Restaurant.find_within(0.1, :origin => session[:geo_location], :conditions => {:closed => 0})
+            near = near[rand(near.size)]
+            p near
+        end
     end
     
 end
